647810b5bbac6193f059f96207b5cae96ecb067c
harish-codewave <harish.k@codewave.com>
2021-08-20 10:44:16 +0530

    fix layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Layout from './Layout/Layout';
-import routes  from './Routes';
-
+import routes from './Routes';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
     return (
         <BrowserRouter>
-            <Layout>
-                <Switch>
-                    {routes.map((data) => (
-                        <Route
-                            key={data.key}
-                            exact
-                            path={data.path}
-                            component={data.component}
-                        />
-                    ))}
-                </Switch>
-            </Layout>
+            <Switch>
+                {routes.map((data) => (
+                    <Route
+                        key={data.key}
+                        exact
+                        path={data.path}
+                        component={data.component}
+                    />
+                ))}
+            </Switch>
         </BrowserRouter>
     );
 }
