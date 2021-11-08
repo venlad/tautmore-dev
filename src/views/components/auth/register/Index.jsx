563901f0d36e72397e4c49baee6738707948f3f5
harish-codewave <harish.k@codewave.com>
2021-11-08 14:54:21 +0530

    register_design

--- src/views/components/auth/register/Index.jsx
+++ src/views/components/auth/register/Index.jsx
@@ -10,13 +10,15 @@ import Mydetails from './Mydetails';
 import Createstudentaccount from './Createstudentaccount';
 import Success from './Success';
 import {
-    chevRight, select, student, parents,
+    chevRight, select, student, parents, errowRight,
 } from '../../../../assets/icons/IconList';
 import { register } from '../../../../stores/Auth/AuthAction';
 import RegisterOption from './RegisterOption';
 
 const Register = ({ registerAction }) => {
     const [step, setStep] = useState(0);
+    const [userType, setUserType] = useState('');
+
     const completeFromStep = () => {
         const data = {
             studentName: 'mithun palangotu',
@@ -40,6 +42,13 @@ const Register = ({ registerAction }) => {
     const backStep = () => {
         setStep((cur) => cur - 1);
     };
+
+    const selectForm = () => {
+        if (userType !== '') {
+            setStep(1);
+        }
+    };
+
     return (
         <div className={`register-main ${step === 5 && 'success'}`}>
             {step < 5 && (
@@ -52,7 +61,7 @@ const Register = ({ registerAction }) => {
                         </div>
                         <div className="col-md-8 col-sm-12">
                             {step > 0 && step < 5 && (
-                                <h2>You are registering as a <span>Parent</span></h2>
+                                <h2>You are registering as a <span>{userType}</span></h2>
                             )}
                         </div>
                     </div>
@@ -64,61 +73,62 @@ const Register = ({ registerAction }) => {
                     <div className="option-part-head">
                         <h3>Sign up!</h3>
                         <p>Join our platform as one of the above</p>
-                        <div className="text-center">
-                            <RegisterOption icon={student} title="Student" />
-                            <RegisterOption icon={parents} title="Parent" />
-                        </div>
+                    </div>
+                    <div className="text-center">
+                        <RegisterOption icon={student} title="Student" setUserType={setUserType} userType={userType} />
+                        <RegisterOption icon={parents} title="Parent" setUserType={setUserType} userType={userType} />
+                        <h5 className="already-account">Already have an account? <Link to="/login">Login here</Link></h5>
+                        <button type="button" onClick={selectForm}>Next {errowRight}</button>
                     </div>
                 </div>
             )}
 
-            {step > 0 && step < 5 && (
-                <div className="stepper-top text-center">
-                    {stepdata.map((data) => (
-                        <div
-                            key={data.title}
-                            className={`stepper-above ${data.id === step ? 'active' : ''} ${
-                                data.id < step ? 'prev-step' : ''
-                            }`}
-                        >
-                            <button type="button">{select}</button>
-                            <span>{data.title}</span>
-                        </div>
-                    ))}
-                </div>
-            )}
+            {step > 0 && step < 5 && userType !== ''  && (
+                <div>
+                    <div className="stepper-top text-center">
+                        {stepdata.map((data) => (
+                            <div
+                                key={data.title}
+                                className={`stepper-above ${data.id === step ? 'active' : ''} ${
+                                    data.id < step ? 'prev-step' : ''
+                                }`}
+                            >
+                                <button type="button">{select}</button>
+                                <span> {data.title}</span>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="step-body">
+                        {step === 1 && <Mydetails />}
+                        {step === 2 && <Coursedetail />}
 
-            {step > 0 && step < 5 && (
-
-                <div className="step-body">
-                    {step === 1 && <Coursedetail />}
-                    {step === 2 && <Mydetails />}
-
-                    {step === 3 && (
-                        <div className="step-1">
-                            <h3 className="text-center">Pay fees</h3>
-                        </div>
-                    )}
-
-                    {step === 4 && <Createstudentaccount />}
-
-                    {step === 5 && <Success />}
-
-                    <div className="text-center step-btn-part">
-                        {step > 1 && step < 5 && <button type="button" onClick={backStep}>Back</button>}
-                        {step < 6 && (
-                            <button type="button" onClick={completeFromStep} className="next-button">
-                                {step === 1 && 'Next'}
-                                {step === 2 && 'Proceed to payment'}
-                                {step === 3 && 'Next'}
-                                {step === 4 && 'Submit'}
-                                {step === 5 && 'Login to account'}
-                                <span>{chevRight}</span>
-                            </button>
+                        {step === 3 && (
+                            <div className="step-1">
+                                <h3 className="text-center">Pay fees</h3>
+                            </div>
                         )}
+
+                        {step === 4 && <Createstudentaccount />}
+
+                        {step === 5 && <Success />}
+
+                        <div className="text-center step-btn-part">
+                            {step > 1 && step < 5 && <button type="button" onClick={backStep}>Back</button>}
+                            {step < 6 && (
+                                <button type="button" onClick={completeFromStep} className="next-button">
+                                    {step === 1 && 'Next'}
+                                    {step === 2 && 'Proceed to payment'}
+                                    {step === 3 && 'Next'}
+                                    {step === 4 && 'Submit'}
+                                    {step === 5 && 'Login to account'}
+                                    <span>{chevRight}</span>
+                                </button>
+                            )}
+                        </div>
                     </div>
                 </div>
             )}
+
         </div>
     );
 };
--- src/views/components/auth/register/RegisterOption.jsx
+++ src/views/components/auth/register/RegisterOption.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { func, string } from 'prop-types';
 
-const RegisterOption = ({ icon, title }) => (
-    <div className="option-common">
-        {icon}
-        <p>{title}</p>
-    </div>
-);
+const RegisterOption = ({
+    icon, title, setUserType, userType,
+}) => {
+    const selectReg = () => {
+        setUserType(title);
+    };
+    return (
+        <div className={`option-common ${userType === title ? 'active' : ''}`} onClick={selectReg} aria-hidden="true">
+            {icon}
+            <p>{title}</p>
+        </div>
+    );
+};
 
 RegisterOption.propTypes = {
     icon: func.isRequired,
     title: string.isRequired,
+    setUserType: func.isRequired,
+    userType: string.isRequired,
 };
 
 export default RegisterOption;
