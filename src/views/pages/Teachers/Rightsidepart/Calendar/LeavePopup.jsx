fe5601d6cca21e59bbf12266ff14151addf96441
harish-codewave <harish.k@codewave.com>
2022-02-09 14:05:08 +0530

    add leave popup menu

--- src/views/pages/Teachers/Rightsidepart/Calendar/LeavePopup.jsx
+++ src/views/pages/Teachers/Rightsidepart/Calendar/LeavePopup.jsx
@@ -1,84 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal } from 'react-bootstrap';
 import { func, bool } from 'prop-types';
 import { Formik, Form, Field } from 'formik';
 import './calendar.scss';
 import { leaveValidation } from './mockData/calendar';
 
-const LeavePopup = ({ model, handleModel }) => (
-    <Modal
-        show={model}
-        onHide={() => handleModel(false)}
-        className="calendar-leave-popup"
-    >
-        <Modal.Header closeButton>
-            <Modal.Title id="example-modal-sizes-title-lg">
-                <h3>Apply for leave</h3>
-                <p>Please make sure you apply your leaves 1-2 days prior and not on the same day</p>
-            </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <Formik
-                enableReinitialize
-                initialValues={{ fromDate: '', toDate: '', reason: '' }}
-                validationSchema={leaveValidation}
-                // onSubmit={(data) => handleAddSubmit(data)}
-                validator={() => ({})}
-            >
-                {({
-                    errors,
-                    touched,
-                    values,
-                    handleSubmit,
-                    handleChange,
-                }) => (
-                    <Form onSubmit={handleSubmit}>
-                        <div className="label">From*</div>
-                        <Field
-                            type="date"
-                            name="fromDate"
-                            onChange={handleChange}
-                            value={values.fromDate}
-                        /><br /> <br />
-                        {errors.fromDate && touched.fromDate ? (
-                            <div className="mt-25 ml-25 rp-manage-school_error-message">
-                                {errors.fromDate}
+const LeavePopup = ({ model, handleModel }) => {
+    const [totalLeave, setTotalLeave] = useState('');
+
+    const fromDateChange = (e, setFieldValue, values) => {
+        setFieldValue('fromDate', e.target.value);
+        const oneDay = 24 * 60 * 60 * 1000;
+        const firstDate = new Date(e.target.value.replace(/-/g, ', '));
+        const secondDate = new Date(values?.toDate.replace(/-/g, ', '));
+        const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
+        if (!Number.isNaN(diffDays)) {
+            setTotalLeave(diffDays);
+        }
+    };
+    const toDateChange = (e, setFieldValue, values) => {
+        setFieldValue('toDate', e.target.value);
+        const oneDay = 24 * 60 * 60 * 1000;
+        const firstDate = new Date(values?.fromDate.replace(/-/g, ', '));
+        const secondDate = new Date(e.target.value.replace(/-/g, ', '));
+        const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
+        if (!Number.isNaN(diffDays)) {
+            setTotalLeave(diffDays);
+        }
+    };
+    return (
+        <Modal
+            show={model}
+            onHide={() => handleModel(false)}
+            className="calendar-leave-popup"
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="example-modal-sizes-title-lg">
+                    <h3>Apply for leave</h3>
+                    <p>Please make sure you apply your leaves
+                        1-2 days prior and not on the same day
+                    </p>
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Formik
+                    enableReinitialize
+                    initialValues={{ fromDate: '', toDate: '', reason: '' }}
+                    validationSchema={leaveValidation}
+                    // onSubmit={(data) => handleAddSubmit(data)}
+                    validator={() => ({})}
+                >
+                    {({
+                        errors,
+                        touched,
+                        values,
+                        handleSubmit,
+                        handleChange,
+                        setFieldValue,
+                    }) => (
+                        <Form onSubmit={handleSubmit}>
+                            <div className="label">From*</div>
+                            <Field
+                                type="date"
+                                name="fromDate"
+                                onChange={(e) => fromDateChange(e, setFieldValue, values)}
+                                value={values.fromDate}
+                            /><br /> <br />
+                            {errors.fromDate && touched.fromDate ? (
+                                <div className="mt-25 ml-25 rp-manage-school_error-message">
+                                    {errors.fromDate}
+                                </div>
+                            ) : null}
+                            <div className="label">To*</div>
+                            <Field
+                                type="date"
+                                name="toDate"
+                                onChange={(e) => toDateChange(e, setFieldValue, values)}
+                                value={values.toDate}
+                            /><br /> <br />
+                            {errors.toDate && touched.toDate ? (
+                                <div className="mt-25 ml-25 rp-manage-school_error-message">
+                                    {errors.toDate}
+                                </div>
+                            ) : null}
+                            <div className="label">Reason*</div>
+                            <Field
+                                type="textarea"
+                                name="reason"
+                                onChange={handleChange}
+                                value={values.reason}
+                            /><br /> <br />
+                            {errors.reason && touched.reason ? (
+                                <div className="mt-25 ml-25 rp-manage-school_error-message">
+                                    {errors.reason}
+                                </div>
+                            ) : null}
+                            <p className="day-leave">{ totalLeave && `${totalLeave} days leave`}</p>
+                            <div className="text-center">
+                                <button type="submit" className="button-common">Apply leave</button>
+                                {/* <Button content="Apply leave" /> */}
                             </div>
-                        ) : null}
-                        <div className="label">To*</div>
-                        <Field
-                            type="date"
-                            name="toDate"
-                            onChange={handleChange}
-                            value={values.toDate}
-                        /><br /> <br />
-                        {errors.toDate && touched.toDate ? (
-                            <div className="mt-25 ml-25 rp-manage-school_error-message">
-                                {errors.toDate}
-                            </div>
-                        ) : null}
-                        <div className="label">Reason*</div>
-                        <Field
-                            type="textarea"
-                            name="reason"
-                            onChange={handleChange}
-                            value={values.reason}
-                        /><br /> <br />
-                        {errors.reason && touched.reason ? (
-                            <div className="mt-25 ml-25 rp-manage-school_error-message">
-                                {errors.reason}
-                            </div>
-                        ) : null}
-                        <div className="text-center">
-                            <button type="submit" className="button-common">Apply leave</button>
-                            {/* <Button content="Apply leave" /> */}
-                        </div>
-                    </Form>
-                )}
-            </Formik>
-        </Modal.Body>
-    </Modal>
-);
+                        </Form>
+                    )}
+                </Formik>
+            </Modal.Body>
+        </Modal>
+    );
+};
 
 LeavePopup.propTypes = {
     handleModel: func.isRequired,
