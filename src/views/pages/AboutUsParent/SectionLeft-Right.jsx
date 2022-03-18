ad7eaec61695f27ccb96231f7f484ba569917759
venkaCivsav <venka.civilizedsavage@gmail.com>
2022-03-18 22:44:19 +0530

    responsive of parent page

--- src/views/pages/AboutUsParent/SectionLeft-Right.jsx
+++ src/views/pages/AboutUsParent/SectionLeft-Right.jsx
@@ -7,55 +7,59 @@ import Button from '../home/Button';
 const SectionLeftRight = ({ className, fromLeft, data }) => (
     <>
         <div className={className}>
-            {
-                fromLeft ? (
-                    <div className="row">
-                        <div className="col-7">
-                            <div className="section-left">
-                                <span className="shade-1" />
-                                <span className="shade-2" />
-                                <span className="shade-3" />
-                                <span className="shade-4" />
-                                <h2>{data?.heading}</h2>
-                                <div dangerouslySetInnerHTML={{ __html: data?.description }} />
-                                <Button content={data?.buttonText} link={data?.buttonUrl} />
-                            </div>
-                        </div>
-                        <div className="col-5">
-                            <div className="section-right">
-                                <span className="shade-1" />
-                                <span className="shade-2" />
-                                <span className="shade-3" />
-                                <span className="shade-4" />
-                                <img src={STRAPI_URL + data?.image?.data?.attributes?.url} alt="" />
-                            </div>
+            {fromLeft ? (
+                <div className="row">
+                    <div className="col-12 col-md-7">
+                        <div className="section-left">
+                            <span className="shade-1" />
+                            <span className="shade-2" />
+                            <span className="shade-3" />
+                            <span className="shade-4" />
+                            <h2>{data?.heading}</h2>
+                            <div dangerouslySetInnerHTML={{ __html: data?.description }} />
+                            <Button content={data?.buttonText} link={data?.buttonUrl} />
                         </div>
                     </div>
-                ) : (
-                    <div className="row">
-                        <div className="col-5">
-                            <div className="section-left-1">
-                                <span className="shade-1" />
-                                <span className="shade-2" />
-                                <span className="shade-3" />
-                                <span className="shade-4" />
-                                <img src={STRAPI_URL + data?.image?.data?.attributes?.url} alt="" />
-                            </div>
-                        </div>
-                        <div className="col-7">
-                            <div className="section-right-1">
-                                <span className="shade-1" />
-                                <span className="shade-2" />
-                                <span className="shade-3" />
-                                <span className="shade-4" />
-                                <h2>{data?.heading}</h2>
-                                <div dangerouslySetInnerHTML={{ __html: data?.description }} />
-                                <Button content={data?.buttonText} link={data?.buttonUrl} />
-                            </div>
+                    <div className="col-12 col-md-5">
+                        <div className="section-right">
+                            <span className="shade-1" />
+                            <span className="shade-2" />
+                            <span className="shade-3" />
+                            <span className="shade-4" />
+                            <img
+                                src={STRAPI_URL + data?.image?.data?.attributes?.url}
+                                alt=""
+                            />
                         </div>
                     </div>
-                )
-            }
+                </div>
+            ) : (
+                <div className="row">
+                    <div className="col-12 col-md-5">
+                        <div className="section-left-1">
+                            <span className="shade-1" />
+                            <span className="shade-2" />
+                            <span className="shade-3" />
+                            <span className="shade-4" />
+                            <img
+                                src={STRAPI_URL + data?.image?.data?.attributes?.url}
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-7">
+                        <div className="section-right-1">
+                            <span className="shade-1" />
+                            <span className="shade-2" />
+                            <span className="shade-3" />
+                            <span className="shade-4" />
+                            <h2>{data?.heading}</h2>
+                            <div dangerouslySetInnerHTML={{ __html: data?.description }} />
+                            <Button content={data?.buttonText} link={data?.buttonUrl} />
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     </>
 );
